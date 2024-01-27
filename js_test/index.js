07cdb5e8587eabd22b093ad8bcedabfa27610724
lgwqwer <lgwqwer@gmail.com>
2024-01-27 20:06:53 +0900

    js 테스트

---
+++ js_test/index.js
@@ -0,0 +1,36 @@
+// 객체 배열 생성
+// const coffee = [];
+
+// coffee.push({name: 'Americano'});
+// coffee.push({caffeine: 10});
+
+// console.log(coffee);
+
+
+// 구조 분해 할당
+// const animal = ['dog', 'cat'];
+
+// let [first, second] = animal;
+
+// console.log(animal);
+
+// 함수의 선언
+// function add(a, b){
+//   return a + b;
+// }
+
+// console.log(add(1, 2));
+
+// 화살표 함수(= 람다 식 또는 람다 함수)의 선언
+// const add = (a, b) => {
+//   return a + b;
+// }
+
+// console.log(add(1, 40));
+
+// 람다 함수는 arguments가 없다! 
+// const func = function (){   // func라는 변수에 익명함수를 저장하는 방식을
+//   console.log(arguments);   // 리터럴 방식이라고 함
+// }
+
+// func(1, 2, 3, 4);
