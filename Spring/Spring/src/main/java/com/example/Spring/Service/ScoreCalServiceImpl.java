33c8546f5a36065e5cc0887998dd16f395595318
Inu <dlsdn3066@naver.com>
2024-02-18 19:28:37 +0900

    html 다시 변경

--- Spring/Spring/src/main/java/com/example/Spring/Service/ScoreCalServiceImpl.java
+++ Spring/Spring/src/main/java/com/example/Spring/Service/ScoreCalServiceImpl.java
@@ -7,8 +7,6 @@ public class ScoreCalculationServiceImpl implements ScoreCalculationService {
 
     @Override
     public double calculateConvertedScore(String collage, double grade) {
-        // 여기에 선택한 단과대학과 학점에 따른 환산 점수를 계산하는 로직을 구현합니다.
-        // 간단하게, 환산 점수를 계산하기 위해 90이라는 상수 변환 요소를 사용하겠습니다.
         double conversionFactor = 90;
         return (grade / 4.5) * conversionFactor;
     }
