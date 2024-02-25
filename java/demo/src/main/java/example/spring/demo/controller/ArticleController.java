87bbc56c3fc9b4aa2bcb136deea146e8146d1164
yeeeab <seaybyb786@gmail.com>
2024-02-25 17:16:14 +0900

    index.html 변경 및 thymeleaf로 데이터 이동 시도 (문제: 사이트에 연결할 수 없음)

--- java/demo/src/main/java/example/spring/demo/controller/ArticleController.java
+++ java/demo/src/main/java/example/spring/demo/controller/ArticleController.java
@@ -2,10 +2,10 @@ package example.spring.demo.controller;
 
 import example.spring.demo.dto.ArticleForm;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ArticleController {
@@ -19,15 +19,38 @@ public class ArticleController {
     @PostMapping("/result")
     @ResponseBody
     public String handleFormSubmission(ArticleForm form){
-        String grade = form.getGrade();
-        String distScore = form.getDistScore();
+        double grade = form.getGrade();
+        int bonusPoint = form.getBonusPoint();
+        int minusPoint = form.getMinusPoint();
+        double distanceScore = form.getDistScore();
 
-        double result = (Double.parseDouble(grade)/4.5f)*90 + Double.parseDouble(distScore);
+        double conversionScore = ((grade + (bonusPoint * 0.009 - minusPoint * 0.009)) / 4.5) * 90 + distanceScore;
+
 
         System.out.println("Received Grade: " + grade);
-        System.out.println("Received Distance Score: " + distScore);
+        System.out.println("Received Bonus Point: " + bonusPoint);
+        System.out.println("Received Minus Point: "+ minusPoint);
+        System.out.println("Received Distance Score: " + distanceScore);
 
-        System.out.println("Result: " + result);
+        System.out.println("Result: " + conversionScore);
         return "";
+    }
+
+    //dto get
+    @RequestMapping(value = "/result", method = RequestMethod.GET)
+    public ModelAndView multiformPage(
+            @ModelAttribute("formData") ArticleForm formDto,
+            ModelAndView mav) {
+        mav.addObject("formData", formDto);
+        mav.setViewName("/result");
+        return mav;
+    }
+    @RequestMapping(value = "/result", method = RequestMethod.POST)
+    public ModelAndView FormDtoSent(
+            @ModelAttribute("formData") ArticleForm formDto,
+            ModelAndView mav){
+        mav.addObject("formData", formDto);
+        mav.setViewName("request");
+        return mav;
     }
 }
--- java/demo/src/main/java/example/spring/demo/dto/ArticleForm.java
+++ java/demo/src/main/java/example/spring/demo/dto/ArticleForm.java
@@ -2,18 +2,40 @@ package example.spring.demo.dto;
 public class ArticleForm {
 
     //index.html 속 form의 name과 동일하게 입력
-    private String grade;
-    private String distscore;
+    private String collage;
+    private double grade;
+    private int bonusPoint;
+    private int minusPoint;
+    private double distanceScore;
+    private String gender;
+    private double conversionScore;
 
-    public ArticleForm(String grade, String distscore) {
+    static boolean isStringEmpty(String str) {return str == null || str.isEmpty();}
+
+    public ArticleForm(String collage, double grade, int bonusPoint, int minusPoint, double distanceScore, String gender) {
+        this.collage = collage;
         this.grade = grade;
-        this.distscore = distscore;
+        this.distanceScore = distanceScore;
+        this.bonusPoint = bonusPoint;
+        this.minusPoint = minusPoint;
+        this.gender = gender;
     }
-    public String getGrade() {
-        return grade;
-    }
-    public String getDistScore() {
-        return distscore;
-    }
+
+    public String getCollage() {return collage;}
+    public double getGrade() {return grade;}
+    public int getBonusPoint() {return bonusPoint;}
+    public int getMinusPoint() {return minusPoint;}
+    public double getDistScore() {return distanceScore;}
+    public String getGender() { return gender;}
+
+    public void setCollage(String collage) { this.collage = collage;}
+    public void setGrade(double grade) { this.grade = grade;}
+    public void setBonusPoint(int bonusPoint) {this.bonusPoint = bonusPoint;}
+    public void setMinusPoint(int minusPoint){this.minusPoint = minusPoint;}
+    public void setDistanceScore(double distanceScore){this.distanceScore = distanceScore;}
+    public void setGender(String gender) { this.gender = gender;}
+    public void setConversionScore(double conversionScore){this.conversionScore = conversionScore;}
+
+
 
 }
