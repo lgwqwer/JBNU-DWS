a30a9fbd53637f280bffa68deeb61766f95cde95
yeeeab <seaybyb786@gmail.com>
2024-03-03 19:02:48 +0900

    데이터베이스 시도

---
+++ java/demo/src/main/java/example/spring/demo/db/DBManager.java
@@ -0,0 +1,16 @@
+package example.spring.demo.db;
+
+import java.sql.Connection;
+
+public class DBManager {
+    String url = "jdbc:mysql://localhost:3306/jbnudws";
+    String userName = "root";
+    String password = "0000";
+
+    public DBManager(String prov, String ct){
+        Connection connection = null;
+        
+
+    }
+
+}
